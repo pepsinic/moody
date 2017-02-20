4ff800d7f5317b24fea76c1d376bb59003e4295e
Your Name <macbook@macbooks-MacBook-Pro.local>
2017-02-20 17:01:06 +0100

    Added emotion.js

---
+++ moody/models/emotion.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+var numberSchema = new Schema({
+    userID:    {type: number, required: true}
+})
+
+var timeSchema = new Schema({
+	userID: {type: Date, default:(new Date()).getTime()
+	})
+
+mongoose.model("sessionCollection", sessionSchema)
