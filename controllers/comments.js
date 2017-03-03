85c9f8733b55138aca39dd969bf45c26782b81ed
Nicole <n.verhagen@elium.academy>
2017-03-03 14:00:16 +0100

    sprint 1 completed

--- controllers/comments.js
+++ controllers/comments.js
@@ -17,24 +17,40 @@ router.post("/:feelingID", (req, res) => {  //works on getting username, time of
  	console.log("getting the _id of that emotion: " + username, userID, feelingID) 
 	
 	emotionM.find({_id:feelingID}, function(error, feelingRecords) { 
-	
-		console.log(feelingRecords[0].time)
-    	res.render("comment", {userID: res.locals.user._id, username : res.locals.user.username, time: feelingRecords[0].time, emotionNum: feelingRecords[0].number})
+		var comment = (feelingRecords[0].comment || "")
+		console.log(feelingRecords)
+    	res.render("comment", {userID: res.locals.user._id, username : res.locals.user.username, 
+    		time: feelingRecords[0].time, 
+    		emotionNum: feelingRecords[0].number, 
+    		emotionID : feelingRecords[0]._id,
+    		comment: feelingRecords[0].comment})
 	})
 })
 
 
 
-// router.post("/create", (req, res) => { //
-// 	userID = res.locals.user._id
-// 	// take time to find the right emotion and time !!!!! -------------TO DO  FRIDAY
-// 	var newComment = req.body.comment
+router.post("/:feelingID/create", (req, res) => { 
+	userID = res.locals.user._id
+	feelingID = req.params.feelingID
+	var newComment = req.body.comment
+	console.log("newComment  :  " + newComment)
 
-// 	console.log("newComment  :  " + newComment)
-	
-// 	emotionM.findOneAndUpdate({userID : userID}).create({})
-// 		res.render("comment", {comments: newComment})
-// }) 
+	emotionM.findByIdAndUpdate({_id:feelingID}, {$set:{comment: newComment}}, function(error, feelingRecords) { 
+		console.log("before newComment: " + newComment)
+        if(error) { //if error ... Show errors in signUp
+            console.log("--*--", error.message)
+            res.render("comment")
+        }
+		feelingRecords.comment = newComment
+		console.log("in records: " + feelingRecords.comment)
+		console.log(feelingRecords)
+    	res.render("comment", {userID: res.locals.user._id, username : res.locals.user.username, 
+    		time: feelingRecords.time, 
+    		emotionNum: feelingRecords.number, 
+    		emotionID : feelingRecords._id, 
+    		comment : feelingRecords.comment})
+	})
+}) 
 
 
 
